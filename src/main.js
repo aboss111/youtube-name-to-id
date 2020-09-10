3a7015db00a891ad3bef2ce22cd57d6c0ee7a211
ABoss <37780520+aboss111@users.noreply.github.com>
2020-09-10 17:31:25 -0400

    First Commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// Import required modules
+const { remote, ipcRenderer } = require('electron')
+
+// minimize window function
+document.getElementById('min').addEventListener('click', () => {
+    remote.getCurrentWindow().minimize()
+})
+
+// close window function
+document.getElementById('close').addEventListener('click', () => {
+    remote.app.quit()
+})
+
+//Get API
+function getAPI() {
+    var channelName = document.getElementById('channel_name').value;
+    var userID = document.getElementById('user_id');
+
+    fetch('https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername='+channelName+'&key=AIzaSyC7XACVZdLgWcicnkWpbP_adG0mXhmdmkI')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            userID.innerHTML = data.items[0].id;
+            userID.style = "float: left; position: relative; top: 15px; right: 80px;"
+            document.getElementById('user_img').src = data.items[0].snippet.thumbnails.default.url;
+            document.getElementById('user_img').style = "border-radius: 50%; float: left; position: relative; top: 15px; width: 50px; margin-right: 10px;";
+        })
+}
